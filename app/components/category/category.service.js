b4a0f3ddbdd89e081adbd7394f9df088d87908fb
khuongtd <khuongtd@ominext.com>
2017-07-26 15:53:26 +0700

    #Category #test api + view

--- app/components/category/category.service.js
+++ app/components/category/category.service.js
@@ -2,13 +2,23 @@
  * Created by vietdd on 29/06/2017.
  */
 (function () {
+    'use strict';
     angular.module("app").service("CategoryService", CategoryService);
-    CategoryService.$inject["HttpService","AppConfig"];
+    CategoryService.$inject = ["$filter", "HttpService", "AppConfig"];
 
-    function CategoryService(){
+    function CategoryService($filter, HttpService, AppConfig){
         this.getListCategory = function (onSuccess, onError) {
-            var url = "";
+            var url = AppConfig.categoryURI.list;
             HttpService.callGetService(url, onSuccess, onError);
-        }
+        };
+
+        this.order = function(data, property){
+            return $filter("orderBy")(data, property);
+        };
+
+        this.delete = function(lstProductId, onSuccess, onError){
+            var url = AppConfig.productURI.deleteList;
+            HttpService.callPutService(url, lstProductId, onSuccess, onError);
+        };
     }
 })();
