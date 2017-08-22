2be0ce80f061312d49ddc6bf34197bad3678f1d3
vietdd <vietdaoduy@gmail.com>
2017-08-22 16:32:29 +0700

    adjust  bill

--- app/components/store/adjust/store.adjust.controller.js
+++ app/components/store/adjust/store.adjust.controller.js
@@ -3,8 +3,102 @@
  */
 (function(){
     'use strict';
-    angular.module('app').controller('StoreAdjustCtrl', [StoreAdjustCtrl]);
-    function StoreAdjustCtrl(){
+    angular.module('app').controller('StoreAdjustCtrl', ['$scope', '$uibModal','StoreService', StoreAdjustCtrl]);
+    function StoreAdjustCtrl($scope, $uibModal, StoreService){
+        var onError = function onError(data){
+            var modal = $uibModal.open({
+                animation: 1,
+                templateUrl: "modalAlertError.html",
+                controller: "ModalInstanceCtrl",
+                size: 'sm'
+            });
+            modal.result.then(function() {
+                $uibModalStack.dismissAll();
+            })
+        }
+
+        var onGetListSuccess = function onSuccess(data){
+            $scope.data = data.value;
+            $scope.stockTrans = $scope.data;
+        }
+        StoreService.getListMaterial("", onGetListSuccess, onError);
+
+        /*select item on autocomplete, and then fill to show data */
+        $scope.total = {
+            storeAmount: 0,
+            adjustAmount: 0,
+            realAmount: 0
+        }
+        $scope.CurrentDate = new Date();
+        $scope.selectedMaterial=[];
+        $scope.onSelect = function(item){
+            //remove from source list
+            var index = $scope.stockTrans.indexOf(item);
+            $scope.stockTrans.splice(index, 1);
+            //reset input data
+            $scope.searchKeyWord = "";
+            // fill to table
+            item.adjust = 0;
+            item.note="";
+            item.realQuantity = item.quantity;
+            item.totalPrice = item.unitPrice*item.adjust;
+            $scope.selectedMaterial.push(item);
+
+            /*calculate  total amount*/
+            $scope.total.storeAmount += item.unitPrice*item.quantity;
+            $scope.total.adjustAmount += item.unitPrice*item.adjust;
+            $scope.total.realAmount += item.unitPrice*item.quantity + item.unitPrice*item.adjust;
+        }
+
+        /*import - open popup*/
+        $scope.chooseFile = function(){
+            var modal = $uibModal.open({
+                animation: 1,
+                templateUrl: "modalChooseFile.html",
+                controller: "ModalInstanceCtrl",
+            });
+            modal.result.then(function() {
+
+            })
+        }
+
+        /*click x button*/
+        $scope.remove = function($index, item){
+            $scope.selectedMaterial.splice($index, 1);
+            $scope.stockTrans.push(item);
+
+
+            $scope.total.realAmount -= item.unitPrice*item.quantity + item.unitPrice*item.adjust;
+            $scope.total.adjustAmount -= item.unitPrice*item.adjust;
+            $scope.total.storeAmount -= item.unitPrice*item.quantity;
+        }
+
+        /*calculate Total*/
+        $scope.calculateTotal = function(value, type, index){
+            if(type){
+                if(!value){
+                    $scope.selectedMaterial[index].unitPrice = 0;
+                }
+                else{
+                    $scope.selectedMaterial[index].unitPrice = $scope.selectedMaterial[index].unitPrice.toString().replace(/[^\d]/g, '');
+                    $scope.selectedMaterial[index].unitPrice = parseInt($scope.selectedMaterial[index].unitPrice, 10);
+                }
+            }
+
+            $scope.total.realAmount = 0;
+            $scope.total.adjustAmount = 0;
+            $scope.total.storeAmount = 0;
+            for(var i=0; i < $scope.selectedMaterial.length; i++){
+                $scope.selectedMaterial[i].totalPrice = $scope.selectedMaterial[i].unitPrice*$scope.selectedMaterial[i].adjust;
+                $scope.selectedMaterial[i].realQuantity = $scope.selectedMaterial[i].adjust + $scope.selectedMaterial[i].quantity;
+
+                $scope.total.adjustAmount += $scope.selectedMaterial[i].unitPrice*$scope.selectedMaterial[i].adjust;
+                $scope.total.storeAmount += $scope.selectedMaterial[i].unitPrice*$scope.selectedMaterial[i].quantity;
+            }
+            $scope.total.adjustAmount = parseInt($scope.total.adjustAmount, 10);
+            $scope.total.storeAmount = parseInt($scope.total.storeAmount, 10);
+            $scope.total.realAmount = $scope.total.adjustAmount + $scope.total.storeAmount;
+        }
 
     }
 
